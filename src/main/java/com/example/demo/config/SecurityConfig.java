75cd4df7ad5c3bd763d258839501088e37c3f716
Lorenzo Snidero <lsnidero@redhat.com>
2025-05-22 15:23:49 +0200

    Relaxed security constraints
    Now every url is accesible.

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,10 +13,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/**","/","/swagger-ui/*","/v3/*").permitAll()
-                        .anyRequest().authenticated())
+        return http.authorizeHttpRequests(auth -> auth
+                        .anyRequest()
+                        .permitAll())
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 }
